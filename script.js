da94ced727de408c049e4f8463a4fc1b8e3bbd38
Y. Vishnu Vardhan Reddy <110284399+Intestelar@users.noreply.github.com>
2025-05-18 08:40:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "28c20bb173e8602e7b716f83082a4821";
+const apiKey = ""; //Add your API key
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input")
